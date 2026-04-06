ff8cb9f7186ea3635a80972d47a7620f757dcaac
Akshat Mani Tripathi <iamak2407@gmail.com>
2026-04-06 19:15:25 +0530

    get all videos

--- video-streaming-system/src/main/java/com/akshat/streaming/repository/VideoRepository.java
+++ video-streaming-system/src/main/java/com/akshat/streaming/repository/VideoRepository.java
@@ -3,6 +3,9 @@ package com.akshat.streaming.repository;
 import com.akshat.streaming.model.Video;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class VideoRepository {
 
@@ -16,4 +19,43 @@ public class VideoRepository {
             e.printStackTrace();
         }
     }
-}
+
+    public ArrayList<Video> getAllVideos() {
+        ArrayList<Video> videoList = new ArrayList<>();
+
+        try {
+            File file = new File(FILE_PATH);
+            Scanner reader = new Scanner(file);
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+
+                if (line.trim().isEmpty()) continue; // skip empty lines
+
+                String[] data = line.split(",");
+
+                // ensure correct format
+                if (data.length != 7) continue;
+
+                int id = Integer.parseInt(data[0]);
+                String title = data[1];
+                String description = data[2];
+                String url = data[3];
+                String creator = data[4];
+                double duration = Double.parseDouble(data[5]);
+                double size = Double.parseDouble(data[6]);
+
+                Video video = new Video(id, description, title, url, creator, duration, size);
+                videoList.add(video);
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            System.out.println("Error reading videos.");
+            e.printStackTrace();
+        }
+
+        return videoList;
+    }
+}  
