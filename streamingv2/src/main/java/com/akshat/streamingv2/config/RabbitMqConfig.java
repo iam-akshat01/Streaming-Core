5a1b0abbe7b44affab0aa9f83f8711fa101b0d10
Akshat Mani Tripathi <iamak2407@gmail.com>
2026-06-13 15:47:13 +0530

    ffmpeg service next

--- streamingv2/src/main/java/com/akshat/streamingv2/config/RabbitMqConfig.java
+++ streamingv2/src/main/java/com/akshat/streamingv2/config/RabbitMqConfig.java
@@ -1,58 +1,71 @@
 package com.akshat.streamingv2.config;
 
-import org.springframework.amqp.core.*;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 
 @Configuration
 public class RabbitMqConfig {
 
-    public static final String EXCHANGE_NAME = "streaming-exchange";
+    public static final String EXCHANGE_NAME =
+            "streaming-exchange";
 
-    public static final String TRANSCODING_QUEUE = "transcoding-queue";
-    public static final String NOTIFICATION_QUEUE = "notification-queue";
+    public static final String TRANSCODING_QUEUE =
+            "transcoding-queue";
 
-    public static final String TRANSCODING_ROUTING_KEY = "video.transcode";
-    public static final String NOTIFICATION_ROUTING_KEY = "video.notification";
+    public static final String NOTIFICATION_QUEUE =
+            "notification-queue";
+
+    public static final String TRANSCODING_ROUTING_KEY =
+            "video.transcode";
+
+    public static final String NOTIFICATION_ROUTING_KEY =
+            "video.notification";
 
     @Bean
     public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
-    return new RabbitAdmin(connectionFactory);
+        return new RabbitAdmin(connectionFactory);
     }
 
     @Bean
     public TopicExchange videoExchange() {
-        System.out.println("Creating exchange bean");
         return new TopicExchange(EXCHANGE_NAME);
     }
 
     @Bean
     public Queue transcodingQueue() {
-        System.out.println("Creating transcoding queue bean");
-        return new Queue(TRANSCODING_QUEUE);
+        return new Queue(TRANSCODING_QUEUE, true);
     }
 
     @Bean
     public Queue notificationQueue() {
-        System.out.println("Creating notification queue bean");
-        return new Queue(NOTIFICATION_QUEUE);
+        return new Queue(NOTIFICATION_QUEUE, true);
     }
 
     @Bean
-    public Binding transcodingBinding() {
+    public Binding transcodingBinding(
+            Queue transcodingQueue,
+            TopicExchange videoExchange) {
+
         return BindingBuilder
-                .bind(transcodingQueue())
-                .to(videoExchange())
+                .bind(transcodingQueue)
+                .to(videoExchange)
                 .with(TRANSCODING_ROUTING_KEY);
     }
 
     @Bean
-    public Binding notificationBinding() {
+    public Binding notificationBinding(
+            Queue notificationQueue,
+            TopicExchange videoExchange) {
+
         return BindingBuilder
-                .bind(notificationQueue())
-                .to(videoExchange())
+                .bind(notificationQueue)
+                .to(videoExchange)
                 .with(NOTIFICATION_ROUTING_KEY);
     }
 }
