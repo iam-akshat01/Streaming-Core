27df92e639d40fb18b1e5fd1c4ff1d58cf1df4c3
Akshat Mani Tripathi <iamak2407@gmail.com>
2026-06-19 11:36:29 +0530

    tried and tested pipeline

--- transcoding-worker/src/main/java/com/akshat/transcodingworker/consumer/VideoProcessingConsumer.java
+++ transcoding-worker/src/main/java/com/akshat/transcodingworker/consumer/VideoProcessingConsumer.java
@@ -5,15 +5,27 @@ import org.springframework.stereotype.Component;
 
 import com.akshat.transcodingworker.config.RabbitMqConfig;
 import com.akshat.transcodingworker.dto.message.VideoProcessingMessage;
+import com.akshat.transcodingworker.service.VideoProcessingService;
 
 @Component
 public class VideoProcessingConsumer {
+
+    private final VideoProcessingService videoProcessingService;
+
+    public VideoProcessingConsumer(
+            VideoProcessingService videoProcessingService) {
+
+        this.videoProcessingService = videoProcessingService;
+    }
 
     @RabbitListener(
             queues = RabbitMqConfig.TRANSCODING_QUEUE)
     public void processVideo(
             VideoProcessingMessage message) {
 
-        System.out.println(message);
+        System.out.println(
+                "Received message: " + message);
+
+        videoProcessingService.processVideo(message);
     }
 }
